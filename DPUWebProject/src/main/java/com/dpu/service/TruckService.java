8da694146c163e9ff88e1cd2f82e9ad7971659e3
jagvir singh <erjagvirsingh92@gmail.com>
2017-02-08 16:30:11 +0530

    Regardin Acknowledgment of Truck

--- DPUWebProject/src/main/java/com/dpu/service/TruckService.java
+++ DPUWebProject/src/main/java/com/dpu/service/TruckService.java
@@ -2,22 +2,19 @@ package com.dpu.service;
 
 import java.util.List;
 
-import com.dpu.entity.Truck;
-import com.dpu.model.DivisionReq;
-import com.dpu.model.DriverReq;
 import com.dpu.model.TruckResponse;
 
 public interface TruckService {
 
-	List<TruckResponse> update(Long id, TruckResponse truckResponse);
+	Object update(Long id, TruckResponse truckResponse);
 
-	List<TruckResponse> delete(Long id);
+	Object delete(Long id);
 
 	TruckResponse get(Long id);
 
 	List<TruckResponse> getAllTrucks(String owner);
 
-	List<TruckResponse> add(TruckResponse truckResponse);
+	Object add(TruckResponse truckResponse);
 	
 	TruckResponse getOpenAdd();
 
